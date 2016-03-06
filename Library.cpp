422f18410370d78314092dcc5c816de8495ea46f
TekuConcept <electro_madness@yahoo.com>
2016-03-06 16:34:31 -0700

    New Test Files

---
+++ Library.cpp
@@ -0,0 +1,9 @@
+#include "Library.h"
+
+void Library::sayHello() {
+	std::cout << "Hello World!";
+}
+
+void Library::doMath(int a, int b) {
+	std::cout << a << " + " << b << " = " << (a+b) << "\n";
+}
---
+++ Library.h
@@ -0,0 +1,7 @@
+#include <iosteam>
+
+public class Library {
+	public:
+		void sayHello();
+		void doMath(int a, int b);
+};
